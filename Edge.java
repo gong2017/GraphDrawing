deb061107f54c5da989894319c5a2636cb5317a5
Straithe <iamstraithe@gmail.com>
2016-04-18 14:08:40 -0500

    Add Edge class, add Vector class

---
+++ Edge.java
@@ -0,0 +1,57 @@
+import java.awt.Color;
+import java.awt.Graphics;
+
+public class Edge
+{
+    private Vertex firstEdgeVertex;
+    private Vertex secondEdgeVertex;
+    private double length;
+
+    // Draws the edge on the screen
+    public void Draw(Graphics g)
+    {      
+        g.setColor(Color.black);
+        g.drawLine((int)(firstEdgeVertex.getPosition().getX()), 
+                   (int)(firstEdgeVertex.getPosition().getY()),
+                   (int)(secondEdgeVertex.getPosition().getX()),
+                   (int)(secondEdgeVertex.getPosition().getY()));
+    }
+    
+    // Sets the edge length
+    public void setLength() {
+        length = firstEdgeVertex.getPosition().subtract(secondEdgeVertex.getPosition()).getLength();
+    }
+
+    // Gets the "spring" of the edge
+    public Vector getForce(Vertex springVertex)
+    {
+        Vector force;
+        double temperature;
+        double cooling;
+
+        if(firstEdgeVertex == springVertex) {
+            force = secondEdgeVertex.getPosition().subtract(firstEdgeVertex.getPosition());
+        }
+        else
+        {
+            force = firstEdgeVertex.getPosition().subtract(secondEdgeVertex.getPosition());
+        }
+
+        temperature = force.getLength() - 10 * length;
+        cooling = Math.signum(temperature) * Math.log( Math.abs(temperature)) * 0.01;
+        force = force.getUnitVector().multiply(cooling);
+        
+        return force;
+    }
+
+    // Sets the first vertex of the edge
+    public void setFirstEdgeVertex(Vertex firstEdgeVertex) {
+        this.firstEdgeVertex = firstEdgeVertex;
+    }
+    
+    // Sets the second vertex of the edge
+    public void setSecondEdgeVertex(Vertex secondEdgeVertex) {
+        this.secondEdgeVertex = secondEdgeVertex;
+        setLength();
+    }
+}
---
+++ Vector.java
@@ -0,0 +1,82 @@
+/**
+ * B. Postnikoff
+ * Vector 
+ * 2016-04-17
+ */
+
+public class Vector
+{
+    private double x;
+    private double y;
+    
+    public Vector()
+    {
+        x = 0;
+        y = 0;
+    }
+    
+    public Vector(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+    
+    public Vector add(Vector secondVector) {
+        Vector addVector = new Vector();
+        
+        addVector.setX(x + secondVector.getX());
+        addVector.setY(y + secondVector.getY());
+        
+        return addVector;
+    }    
+    
+    public Vector subtract(Vector secondVector) {
+        Vector subtractVector = new Vector();
+        
+        subtractVector.setX(x - secondVector.getX());
+        subtractVector.setY(y - secondVector.getY());
+        
+        return subtractVector;
+    }   
+    
+    public Vector multiply(double scalar) {
+        Vector multiplyVector = new Vector();
+        
+        multiplyVector.setX(x * scalar);
+        multiplyVector.setY(y * scalar);
+        
+        return multiplyVector;
+    }
+    
+    public double getLength() {
+        double length;
+        
+        length = Math.sqrt((x * x) + (y * y));
+        
+        return length;
+    }
+    
+    public Vector getUnitVector() {
+        Vector unitVector = new Vector();
+        
+        unitVector.setX(x / getLength());
+        unitVector.setY(y / getLength());
+        
+        return unitVector;
+    }
+    
+    public double getX() {
+        return x;
+    }
+    
+    public void setX(double newX) {
+        x = newX;
+    }
+    
+    public double getY() {
+        return y;
+    }
+    
+    public void setY(double newY) {
+        y = newY;
+    }
+}
